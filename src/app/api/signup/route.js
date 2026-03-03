97567a6e1367987a4562af1cafd3607d53a2e4aa
Shariful Islam <sharifulislam242248@gmail.com>
2026-03-04 03:54:45 +0600

    fix done

--- src/app/api/signup/route.js
+++ src/app/api/signup/route.js
@@ -1,15 +1,16 @@
-import  mongoConnect  from "@/lib/mongoConnect";
+import mongoConnect from "@/lib/mongoConnect";
 import bcrypt from "bcryptjs";
 import { NextResponse } from "next/server";
-
+import mongoose from "mongoose"; 
 export async function POST(req) {
   try {
     const { name, username, email, password, image } = await req.json();
-    if (!name || !username || !email || !password) {
-      return NextResponse.json({ error: "সবগুলো ঘর পূরণ করুন!" }, { status: 400 });
-    }
 
-    const { db } = await mongoConnect();
+    if (!name || !username || !email || !password) {
+      return NextResponse.json({ error: "All fields are required!" }, { status: 400 });
+    }
+    await mongoConnect();
+    const db = mongoose.connection.db; 
 
     
     const userExists = await db.collection("users").findOne({
@@ -23,7 +24,6 @@ export async function POST(req) {
       return NextResponse.json({ error: "Email or Username already exists!" }, { status: 400 });
     }
 
-   
     const hashedPassword = await bcrypt.hash(password, 10);
 
     const newUser = {
@@ -36,12 +36,13 @@ export async function POST(req) {
       createdAt: new Date(),
     };
 
+
     await db.collection("users").insertOne(newUser);
 
     return NextResponse.json({ message: "User registered successfully!" }, { status: 201 });
 
   } catch (error) {
     console.error("Signup Error:", error);
-    return NextResponse.json({ error: "Internal Server Error" }, { status: 500 });
+    return NextResponse.json({ error: error.message || "Internal Server Error" }, { status: 500 });
   }
 }
